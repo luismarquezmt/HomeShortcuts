0d61711e6bf8e0189138ccb52469f169c8ba4ede
Luis <luis.marquez.mt@usa.edu.co>
2023-10-18 17:48:39 -0500

    thirdImprovement

--- README.md
+++ README.md
@@ -1,8 +1,21 @@
-Version 1.1 
 
-Se añade nuevas cards en bootstrap5 con enlaces de Python y Javascript.
-Se elimina enlaces fuera de servicio
+
+Home Shortcuts Python Javascript HTML CSS in Bootstrap5 
 
 ```sh
-Home Shortcuts from Python Javascript HTML CSS in Bootstrap5 
+
+
+Version 1.0
+
+16/12/22 Se crea proyecto paginaweb HTML estatica con bootstrap para agilizar acceso a aplicativos y enlaces utiles 
+
+Version 1.0.1 
+
+17/10/23 Se añade nuevas cards en bootstrap5 con enlaces de Python y Javascript.
+17/10/23 Se elimina enlaces fuera de servicio
+
+Version 1.0.2 
+
+18/10/23 Se añade acordion con información de Python, se ajusta css, se crea boton de copiar para agilizar el acceso a comados python 
+
 ```
--- python.js
+++ python.js
@@ -1,12 +1,33 @@
 
+
+let totalStatement = document.getElementsByClassName("code-copy")
+let niuwMap = new Map()
+
+for (let statement of totalStatement) {
+  let codeToCopy
+  let badge
+  badge = statement.children[1];
+  codeToCopy = statement.children[0].innerHTML
+  niuwMap.set(badge, codeToCopy)
+}
+
+niuwMap.forEach(function (value, key) {
+  key.addEventListener("click", () => {
+    navigator.clipboard.writeText(value)
+  })
+})
+
+
+
+
 class card {
-    constructor(idCard, cardName, imgSrc, link) {
-        this.idCard = idCard
-        this.cardName = cardName
-        this.img = new Image
-        this.img.src = imgSrc
-        this.link = link
-    }
+  constructor(idCard, cardName, imgSrc, link) {
+    this.idCard = idCard
+    this.cardName = cardName
+    this.img = new Image
+    this.img.src = imgSrc
+    this.link = link
+  }
 }
 
 
@@ -16,16 +37,17 @@ let pypi = new card("Pypi", "The Python Package Index (PyPI) is a repository of 
 let python = new card("Python", "Python is a programming language that lets you work quickly and integrate systems more effectively", "src/images/python.png", "https://www.python.org/")
 let uvicorn = new card("uvicorn", "Uvicorn is an ASGI web server implementation for Python.", "src/images/uviCorn.png", "https://www.uvicorn.org/")
 let fastapi = new card("FastApi", "FastAPI framework, high performance, easy to learn, fast to code, ready for production", "src/images/fastApi.png", "https://fastapi.tiangolo.com/")
-let pandas = new card("Pandas", "pandas is an open source, BSD-licensed library providing high-performance, easy-to-use data structures and data analysis tools for the Python programming language.", "src/images/pandas.png", "https://pandas.pydata.org/pandas-docs/stable/index.html")
+let pandas = new card("Pandas", "BSD-licensed library providing high-performance, easy-to-use data structures and data analysis tools for Python.", "src/images/pandas.png", "https://pandas.pydata.org/pandas-docs/stable/index.html")
 let collab = new card("Collab", "Google Collab", "src/images/collab.png", "https://colab.research.google.com/")
+let maplotlib = new card("Maplotlib", "Matplotlib is a comprehensive library for creating static, animated, and interactive visualizations.", "src/images/matplotlib.png", "https://matplotlib.org/stable/")
 
 
-cards.push(pypi, python, uvicorn, fastapi, pandas,collab)
+cards.push(pypi, python, uvicorn, fastapi, maplotlib, pandas, collab)
 
 cards.forEach(card => {
-    let spanCard = `
+  let spanCard = `
     <div id="${card.idCard}" class="col">
-      <div class="card">
+      <div class="card" style="height: 100%;>
         <a href="${card.link}" target="_blank">
           <img src="${card.img.src}" class="card-img-top" alt="..."></img> </a>
         <div class="card-body">
@@ -35,7 +57,7 @@ cards.forEach(card => {
       </div>
     </div>`
 
-    document.getElementById("span-cards-space").innerHTML += spanCard
+  document.getElementById("span-cards-space").innerHTML += spanCard
 
 });
 
